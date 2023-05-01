2163dae0ef1e64e5fd74f5ef0a4be1ec48a504dd
Aziz Halilov <poplyq@live.com>
2023-05-01 23:21:49 +0300

    feat: add buttons and functions

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const keyboardContainer = document.createElement('div');
 keyboardContainer.classList.add('keyboard-container');
 
 const keyboardKeys = [
-  ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']'],
-  ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", '\\'],
-  ['`', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/'],
+  ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']'],
+  ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", '\\'],
+  ['Shift', '`', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'au', 'shift'],
 ];
 const digitKey = [
   '§',
@@ -21,10 +21,10 @@ const digitKey = [
   '-',
   '=',
 ];
-//  const spesialKeys = ['-', '=', '[', ']', ';', `'`, '\\', ',', '.', '/'];
+
 
 const displayValue = [];
-const display = document.createElement('div');
+const display = document.createElement('textarea');
 display.classList.add('display');
 keyboardContainer.appendChild(display);
 
@@ -70,6 +70,14 @@ keyboardKeys.forEach((row) => {
     const keyElement = document.createElement('button');
     keyElement.classList.add('keyboard-key');
     keyElement.innerText = key;
+    keyElement.addEventListener('click', () => {
+      displayValue.push(key);
+      display.innerHTML = displayValue.join('');
+      keyElement.classList.toggle('activeBtn');
+      setTimeout(() => {
+        keyElement.classList.toggle('activeBtn');
+      }, 100);
+    });
     switch (key) {
       case '-':
         keyElement.code = 'Minus';
@@ -108,44 +116,114 @@ keyboardKeys.forEach((row) => {
         keyElement.code = `Key${key.toUpperCase()}`;
         break;
     }
-
-    keyElement.addEventListener('click', () => {
-      displayValue.push(key);
-      display.innerHTML = displayValue.join('');
-      keyElement.classList.toggle('activeBtn');
-      setTimeout(() => {
-        keyElement.classList.toggle('activeBtn');
-      }, 100);
-    });
-
     rowElement.appendChild(keyElement);
   });
-
   keyboardContainer.appendChild(rowElement);
 });
 
-const shift = document.createElement('button');
-shift.classList.add('shift');
-shift.innerText = 'shift';
-shift.addEventListener('click', () => {
-  shift.classList.toggle('activeBtn');
-  setTimeout(() => {
-    shift.classList.toggle('activeBtn');
-  }, 100);
-});
-
-keyboardContainer.appendChild(shift);
 
 document.body.appendChild(keyboardContainer);
 
+const specialKeys = [
+  'control',
+  'option',
+  'cmd',
+  'space',
+  'command',
+  'opt',
+];
+
+specialKeys.forEach((key) => {
+  const keyElement = document.createElement('button');
+  keyElement.classList.add('keyboard-key');
+  keyElement.innerHTML = key;
+  switch (key) {
+    case 'control':
+      keyElement.code = 'ControlLeft';
+      keyElement.classList.add('ControlLeft');
+          break;
+        case 'option':
+          keyElement.code = 'AltLeft';
+          keyElement.classList.add('AltLeft');
+          break;
+        case 'cmd':
+          keyElement.code = 'MetaLeft';
+          keyElement.classList.add('MetaLeft');
+          break;
+        case 'space':
+          keyElement.code = 'Space';
+          keyElement.classList.add('Space');
+          break;
+        case 'command':
+          keyElement.code = 'MetaRight';
+          keyElement.classList.add('MetaRight');
+          break;
+        case 'opt':
+          keyElement.code = 'AltRight';
+          keyElement.classList.add('AltRight');
+          break;
+        case 'ArrowRight':
+          keyElement.code = 'ControlLeft';
+          keyElement.classList.add('ControlLeft');
+          break;
+        case 'ArrowLeft':
+          keyElement.code = 'ArrowLeft';
+          keyElement.classList.add('ArrowLeft');
+          break;
+
+        case 'ArrowDown':
+          keyElement.code = 'ArrowDown';
+          keyElement.classList.add('ArrowDown');
+          break;
+  }
+
+  keyElement.addEventListener('click', () => {
+    keyElement.classList.toggle('activeBtn');
+    setTimeout(() => {
+      keyElement.classList.toggle('activeBtn');
+    }, 100);
+  });
+
+  keyboardContainer.appendChild(keyElement);
+});
+
+const spesialLeft=['Tab', 'Shift', 'CapsLock',]
+spesialLeft.forEach(()=>{
+  const keyElement = document.createElement('button');
+  keyElement.classList.add('keyboard-key');
+  keyElement.innerHTML = key;
+  switch (key) {
+  case 'Tab':
+    keyElement.code = 'Tab';
+    keyElement.classList.add('tab');
+    break;
+  case 'Shift':
+    keyElement.code = 'ShiftLeft';
+    keyElement.classList.add('shiftleft');
+    break;
+  case 'CapsLock':
+    keyElement.code = 'CapsLock';
+    keyElement.classList.add('caps');
+    break;
+  case 'shift':
+    keyElement.code = 'ShiftRight';
+    keyElement.classList.add('shiftright');
+    break;
+    case 'au':
+      keyElement.code = 'ArrowUp';
+      keyElement.classList.add('ArrowUp');
+      break;
+}
+keyboardContainer.appendChild(keyElement);
+}
+)
+
 document.addEventListener('keypress', (e) => {
+  console.log(e);
   const keys = document.querySelectorAll('.keyboard-key');
   const arr = Array.from(keys);
-  console.log(e);
-
   displayValue.push(e.key);
   display.innerHTML = displayValue.join('');
-
   arr.forEach((el) => {
     if (el.code === e.code) {
       el.classList.toggle('activeBtn');
@@ -156,12 +234,80 @@ document.addEventListener('keypress', (e) => {
   });
 });
 document.addEventListener('keydown', (e) => {
-  if (e.shiftKey) {
-    shift.classList.add('activeBtn');
-  }
+console.log(e);
+  toggle(e);
 });
+
 document.addEventListener('keyup', (e) => {
-  if (!e.shiftKey) {
-    shift.classList.remove('activeBtn');
-  }
+  toggle(e);
 });
+
+function toggle(e) {
+  const keys = document.querySelectorAll('.keyboard-key');
+  const arr = Array.from(keys);
+  arr.forEach((el) => {
+    if (el.code === e.code) {
+      switch (el.code) {
+        case 'Tab':
+          el.classList.toggle('activeBtn');
+tab()
+          break;
+        case 'CapsLock':
+          el.classList.toggle('activeBtn');
+          break;
+        case 'ShiftLeft':
+          el.classList.toggle('activeBtn');
+          break;
+        case 'ShiftRight':
+          el.classList.toggle('activeBtn');
+          break;
+        case 'ControlLeft':
+          el.classList.toggle('activeBtn');
+          break;
+        case 'AltLeft':
+          el.classList.toggle('activeBtn');
+          break;
+        case 'MetaLeft':
+          el.classList.toggle('activeBtn');
+          break;
+        case 'Space':
+          el.classList.toggle('activeBtn');
+          break;
+        case 'MetaRight':
+          el.classList.toggle('activeBtn');
+          break;
+        case 'AltRight':
+          el.classList.toggle('activeBtn');
+          break;
+        case 'ArrowRight':
+          el.classList.toggle('activeBtn');
+          break;
+        case 'ArrowLeft':
+          el.classList.toggle('activeBtn');
+          break;
+        case 'ArrowUp':
+          el.classList.toggle('activeBtn');
+          break;
+        case 'ArrowDown':
+          el.classList.toggle('activeBtn');
+          break;
+      }
+    }
+  });
+}
+function tab(){
+  for(let i = 0; i<4; i++){
+    displayValue.push(' ');
+    display.innerHTML = displayValue.join('');}
+  
+}
+
+
+function addEventListen (key, element) {
+  displayValue.push(key);
+  display.innerHTML = displayValue.join('');
+  element.classList.toggle('activeBtn');
+  setTimeout(() => {
+    element.classList.toggle('activeBtn');
+  }, 100);
+}
